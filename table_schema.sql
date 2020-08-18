fe1d59d1e63706c4b392a70f270c5d02dc290a0b
Avni <avni@Rohans-Air.attlocal.net>
2020-08-17 20:32:05 -0400

    tableschema

---
+++ table_schema.sql
@@ -0,0 +1,61 @@
+CREATE TABLE Department (
+	dept_number varchar(30) NOT NULL,
+	dept_name varchar (30) NOT NULL,
+	primary key (dept_number)
+	);
+
+select *
+from Department;
+
+CREATE TABLE Employees (
+	emp_no VARCHAR (30) NOT NULL,
+	emp_title_id VARCHAR (30) NOT NULL,
+	birth_date VARCHAR (30) NOT NULL,
+	first_name VARCHAR (30) NOT NULL,
+	Last_name VARCHAR (30) NOT NULL,
+	Sex VARCHAR (30) NOT NULL,
+	Hire_date VARCHAR (30) NOT NULL,
+	PRIMARY KEY (emp_no)
+	);
+	
+
+select *
+from Employees;
+
+CREATE TABLE Dept_manager (
+	dept_number varchar (30) NOT NULL , 
+	emp_no VARCHAR (30) NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
+	FOREIGN KEY (dept_number) REFERENCES Department(dept_number)
+	);
+
+select *
+from Dept_manager;
+
+CREATE TABLE Dept_Emp (
+	emp_no varchar (30) NOT NULL , 
+	dept_name VARCHAR (30) NOT NULL,
+	foreign key (emp_no) references Employees(emp_no) 
+	);
+
+Select *
+from Dept_Emp;
+
+CREATE TABLE Salaries (
+	emp_no VARCHAR (30) NOT NULL,  
+	salary VARCHAR (30) NOT NULL,
+	foreign key (emp_no) references Employees (emp_no)
+	);
+
+select *
+from Salaries;
+
+CREATE TABLE titles (
+	title_id VARCHAR (30) NOT NULL,  
+	title VARCHAR (30) NOT NULL,
+	primary key (title_id)
+	);
+
+select *
+from titles;
+
